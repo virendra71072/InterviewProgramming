f1907f15b24131832f7180497f6d0def6bfb6990
Mukesh kumar gupta <mkshkmrgpt@gmail.com>
2017-04-09 23:32:32 +0530

    invert binay tree

---
+++ InterviewBit/src/Trees/InvertBinayTree.java
@@ -0,0 +1,41 @@
+package Trees;
+
+/*Given a binary tree, invert the binary tree and return it. 
+Look at the example for more details.
+
+Example : 
+Given binary tree
+
+     1
+   /   \
+  2     3
+ / \   / \
+4   5 6   7
+invert and return
+
+     1
+   /   \
+  3     2
+ / \   / \
+7   6 5   4
+*/
+public class InvertBinayTree {
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null)
+      return null;
+    TreeNode tmp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(tmp);
+    return root;
+  }
+}

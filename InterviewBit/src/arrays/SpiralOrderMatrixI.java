998e02682dcef420d9ea4aefbddfb9140c229084
Mukesh kumar gupta <mkshkmrgpt@gmail.com>
2017-04-20 22:15:42 +0530

    Spiral order matixI

---
+++ InterviewBit/src/arrays/SpiralOrderMatrixI.java
@@ -0,0 +1,58 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*Given a matrix of m * n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example:
+Given the following matrix:
+
+[
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+]
+You should return
+[1, 2, 3, 6, 9, 8, 7, 4, 5]*/
+
+public class SpiralOrderMatrixI {
+  public ArrayList<Integer> spiralOrder(final List<ArrayList<Integer>> A) {
+    ArrayList<Integer> result = new ArrayList<Integer>();
+    // Populate result;
+    if (A.size() == 0 || A.get(0).size() == 0) {
+      return result;
+    }
+    int rows = A.size();
+    int cols = A.get(0).size();
+
+    int mid = (rows - 1) / 2;
+    for (int i = 0; i <= mid; i++) {
+      int endCol = cols - i - 1;
+      int endRow = rows - i - 1;
+      // print the first row
+      for (int col = i; col <= endCol; col++) {
+        result.add(A.get(i).get(col));
+      }
+      // print the right most column if exists
+      if (endCol >= i) {
+        for (int row = i + 1; row <= endRow; row++) {
+          result.add(A.get(row).get(endCol));
+        }
+      }
+      // print the bottom row
+      if (endRow > i) {
+        for (int col = endCol - 1; col >= i; col--) {
+          result.add(A.get(endRow).get(col));
+        }
+      }
+      // print the leftmost column
+      if (endCol > i && endRow > i + 1) {
+        for (int row = endRow - 1; row > i; row--) {
+          result.add(A.get(row).get(i));
+        }
+      }
+    }
+    return result;
+  }
+}
